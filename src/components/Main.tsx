06e664f53948e2ffc355b31aefe460d3d0763946
Julian krochmal <jkrochmal.ext@andromedalatam.com>
2021-10-28 14:55:12 -0300

    reviews component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { FaStar, FaLocationArrow } from 'react-icons/fa';
 import hotels from './../json/hotel.json'
 
@@ -38,25 +37,54 @@ const Main = () => {
                 ))}
             </div>
 
-            {hotels ?
-                <div className="overview">
-                    <div className="overview__heading">
-                        {hotels.name}
-                    </div>
-                    <div className="overview__stars">
-                        {getStars(hotels.stars)}
-                    </div>
-                    <div className="overview__location">
-                        <FaLocationArrow className='overview__icon-location' />
-                        <button className="btn-inline">{hotels.location}</button>
-                    </div>
-                    <div className="overview__rating">
-                        <div className="overview__rating-average">{hotels.ratingAverage}</div>
-                        <div className="overview__rating-count">{hotels.ratingCount}</div>
+            <div className="overview">
+                <div className="overview__heading">
+                    {hotels.name}
+                </div>
+                <div className="overview__stars">
+                    {getStars(hotels.stars)}
+                </div>
+                <div className="overview__location">
+                    <FaLocationArrow className='overview__icon-location' />
+                    <button className="btn-inline">{hotels.location}</button>
+                </div>
+                <div className="overview__rating">
+                    <div className="overview__rating-average">{hotels.ratingAverage}</div>
+                    <div className="overview__rating-count">{hotels.ratingCount}</div>
+                </div>
+            </div>
+
+            <div className="detail">
+                <div className="description">
+                    <p className="paragraph">
+                        {hotels.description}
+                    </p>
+                    <p className="paragraph">
+                        {hotels.description}
+                    </p>
+                    <ul className="list">
+                        <li className="list__item">Close to the beach</li>
+                        <li className="list__item">Breakfast included</li>
+                        <li className="list__item">Free airport shuttle</li>
+                        <li className="list__item">Free wifi in all rooms</li>
+                        <li className="list__item">air conditioning and heating</li>
+                        <li className="list__item">Pet friendly</li>
+                        <li className="list__item">Perfect for families</li>
+                    </ul>
+
+                    <div className="recommend">
+                        <div className="recommend__count">
+                            Lucy and 3 other friends recommend this hotel
+                        </div>
+
+                        <div className="recommend__friends">
+                            <img src="./../img/user-1.jpg" alt="Test" />
+                        </div>
                     </div>
                 </div>
-                : <Fragment />
-            }
+                <div className="user-reviews">
+                </div>
+            </div>
 
         </main>
     )
